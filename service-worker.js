a23eb3de2c49968b766e32066418237b555bd5ea
mericci <mericci@uc.cl>
2020-05-16 19:52:03 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mericci/Tarea3-TallerDeIntegracion/precache-manifest.fb7e518ff6414d74b489d578fe0dfa3a.js"
+  "/Tarea3-TallerDeIntegracion/precache-manifest.812f122bf3831c49f070b9776deb4dc0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mericci/Tarea3-TallerDeIntegracion/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Tarea3-TallerDeIntegracion/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
